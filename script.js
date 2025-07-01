b9cb97aa44e25e144141fea0bae3421eb9147160
Gaston <leitnergaston@gmail.com>
2025-07-01 18:36:29 -0300

    nuevo pagina completa

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+//* Año actualizado automaticamente en footer
+document.getElementById('current-year').textContent = new Date().getFullYear();
+});
